f1a2d891a4e008b04e02452e74dd5b43fd2ba0d9
kulp <kulp@stsci.edu>
2022-07-26 13:20:50 -0400

    Fixed docstring error and added context for fits file

--- wss_tools/utils/recenter.py
+++ wss_tools/utils/recenter.py
@@ -1,4 +1,6 @@
 ''' Module to recenter images '''
+
+# STDLIB
 import os
 from astropy.io import fits
 from astropy.nddata import block_reduce
@@ -10,7 +12,8 @@ __all__ = ['recenter']
 
 
 def recenter(images, outputdir, doplot=False):
-    '''Recenter images based on NIRCam XY (464,1412) if offset > 10px
+    """Recenter images based on NIRCam XY (464,1412) if offset > 10px
+
     Parameters
     ----------
     images : list
@@ -24,83 +27,85 @@ def recenter(images, outputdir, doplot=False):
     -------
     output_images : list
         Output images that have been read and/or modified.
-    '''
+
+    """
+
     output_images = []
     for im_fn in images:
         # Open fits
-        hdul = fits.open(im_fn)
-        data = hdul[1].data
+        with fits.open(im_fn) as hdul:
+            data = hdul[1].data
 
-        # Rebin image with very big pixels to find the approximate location
-        size = 32
-        rebindata = block_reduce(data, block_size=64, func=np.median)
+            # Rebin image with very big pixels to find the approximate location
+            size = 32
+            rebindata = block_reduce(data, block_size=64, func=np.median)
 
-        # Remove background
-        rebindata -= np.median(rebindata)
+            # Remove background
+            rebindata -= np.median(rebindata)
 
-        margin_left = 1.5
-        margin_right = 2.5
-        margin_top = 2.5
-        margin_bottom = 1.5
-        imsize = 2048
+            margin_left = 1.5
+            margin_right = 2.5
+            margin_top = 2.5
+            margin_bottom = 1.5
+            imsize = 2048
 
-        # Find the center of mass and cut a box around: we should see the whole
-        # PSF with about 1-2 PSF wide margins
-        com = ndimage.center_of_mass(rebindata)
-        subdata = data[int((com[0] - margin_left) * imsize / size):
-                       int((com[0] + margin_right) * imsize / size),
-                       int((com[1] - margin_bottom) * imsize / size):
-                       int((com[1] + margin_top) * imsize / size)]
+            # Find the center of mass and cut a box around: we should see the whole
+            # PSF with about 1-2 PSF wide margins
+            com = ndimage.center_of_mass(rebindata)
+            subdata = data[int((com[0] - margin_left) * imsize / size):
+                           int((com[0] + margin_right) * imsize / size),
+                           int((com[1] - margin_bottom) * imsize / size):
+                           int((com[1] + margin_top) * imsize / size)]
 
-        # Rebin again
-        rebinsubdata = block_reduce(subdata, block_size=8, func=np.median)
+            # Rebin again
+            rebinsubdata = block_reduce(subdata, block_size=8, func=np.median)
 
-        # Remove background
-        rebinsubdata -= np.median(rebinsubdata)
+            # Remove background
+            rebinsubdata -= np.median(rebinsubdata)
 
-        # Find the center of mass and cut a box around: should see the inside
-        # of the PSF
-        com1 = ndimage.center_of_mass(rebinsubdata)
-        margin_left2 = 0.5
-        margin_right2 = 1.5
-        margin_bottom2 = 0.5
-        margin_top2 = 1.5
-        subdata2 = subdata[int((com1[0] - margin_left2) * imsize / size * 4 / size):
-                           int((com1[0] + margin_right2) * imsize / size * 4 / size),
-                           int((com1[1] - margin_bottom2) * imsize / size * 4 / size):
-                           int((com1[1] + margin_top2) * imsize / size * 4 / size)]
+            # Find the center of mass and cut a box around: should see the inside
+            # of the PSF
+            com1 = ndimage.center_of_mass(rebinsubdata)
+            margin_left2 = 0.5
+            margin_right2 = 1.5
+            margin_bottom2 = 0.5
+            margin_top2 = 1.5
+            subdata2 = subdata[int((com1[0] - margin_left2) * imsize / size * 4 / size):
+                               int((com1[0] + margin_right2) * imsize / size * 4 / size),
+                               int((com1[1] - margin_bottom2) * imsize / size * 4 / size):
+                               int((com1[1] + margin_top2) * imsize / size * 4 / size)]
 
-        # Find the center of mass
-        com2 = ndimage.center_of_mass(subdata2)
+            # Find the center of mass
+            com2 = ndimage.center_of_mass(subdata2)
 
-        xcntr = 464
-        ycntr = 1412
+            xcntr = 464
+            ycntr = 1412
 
-        # Recenter the image to 464, 1412 instead to match the WAS expectations
-        xcpsf = (com2[0] + int((com1[0] - margin_left2) * imsize / size * 4 / size)
-                 + int((com[0] - margin_left) * imsize / size))
-        ycpsf = (com2[1] + int((com1[1] - margin_bottom2) * imsize / size * 4 / size)
-                 + int((com[1] - margin_bottom) * imsize / size))
-        offsetdata = np.roll(data, (xcntr - int(ycpsf),
-                                    ycntr - int(xcpsf)),
-                             axis=(1, 0))
+            # Recenter the image to 464, 1412 instead to match the WAS expectations
+            xcpsf = (com2[0] + int((com1[0] - margin_left2) * imsize / size * 4 / size)
+                     + int((com[0] - margin_left) * imsize / size))
+            ycpsf = (com2[1] + int((com1[1] - margin_bottom2) * imsize / size * 4 / size)
+                     + int((com[1] - margin_bottom) * imsize / size))
+            offsetdata = np.roll(data, (xcntr - int(ycpsf),
+                                        ycntr - int(xcpsf)),
+                                 axis=(1, 0))
 
-        # Save back image into file
-        if abs(xcntr - ycpsf) > 10 or abs(ycntr - xcpsf) > 10:
-            # Do the same for the ERR and the DQ
-            hdul[2].data = np.roll(hdul[2].data,
-                                   (xcntr - int(ycpsf), ycntr - int(xcpsf)),
-                                   axis=(1, 0))
-            hdul[3].data = np.roll(hdul[3].data,
-                                   (xcntr - int(ycpsf), ycntr - int(xcpsf)),
-                                   axis=(1, 0))
-            hdul[1].data = offsetdata
-            filename = os.path.basename(im_fn).replace('.fits',
-                                                       '_recenter.fits')
-            hdul.writeto(os.path.join(outputdir, filename), overwrite=True)
-            output_images.append(os.path.join(outputdir, filename))
-        else:
-            output_images.append(im_fn)
+            # Save back image into file
+            if abs(xcntr - ycpsf) > 10 or abs(ycntr - xcpsf) > 10:
+                # Do the same for the ERR and the DQ
+                hdul[2].data = np.roll(hdul[2].data,
+                                       (xcntr - int(ycpsf), ycntr - int(xcpsf)),
+                                       axis=(1, 0))
+                hdul[3].data = np.roll(hdul[3].data,
+                                       (xcntr - int(ycpsf), ycntr - int(xcpsf)),
+                                       axis=(1, 0))
+                hdul[1].data = offsetdata
+                filename = os.path.basename(im_fn).replace('.fits',
+                                                           '_recenter.fits')
+                hdul.writeto(os.path.join(outputdir, filename), overwrite=True)
+                output_images.append(os.path.join(outputdir, filename))
+            else:
+                output_images.append(im_fn)
 
         # Plot
 
