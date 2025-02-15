75a91b4feea15b02d06a5335dcfe044763fb2fc6
kulp <kulp@stsci.edu>
2025-02-15 08:06:10 -0500

    issue-104-astropy-testrunner-deprecated

--- wss_tools/__init__.py
+++ wss_tools/__init__.py
@@ -6,7 +6,7 @@ Python tools for JWST Wavefront Sensing Software.
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
-from ._astropy_init import *  # noqa
+
 # ----------------------------------------------------------------------------
 
 # UI
